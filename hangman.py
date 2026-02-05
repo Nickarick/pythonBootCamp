0babacc30f36270fd79efa9490fc8ae55c6f9442
Nickarick <78655331+Nickarick@users.noreply.github.com>
2026-02-05 09:49:56 -0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,7 +1,7 @@
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
-
+correct_letters = [""]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
@@ -14,16 +14,29 @@ for guess in range(len(chosen_word)):
 
 print(placeholder)
 
-display = ""
-while chosen_word == display:
+#User enters letter until blanks are filled
+game_over = False
+
+while not game_over:
+
     guess = input("Guess a letter: \n").lower()
+#Display that puts the letter in the right spot
+
+    display = " "
+
     for letter in chosen_word:
         if letter == guess:
-            display += letter   
+            display += letter
+            correct_letters.append(letter)
+
+        elif letter in correct_letters:
+            display += letter
+        
         else:
             display += "_"
 
-print(display)
+    print(display)
 
-
-#for letter in chosen_word:
+    if "_" not in display:
+        game_over = True
+        print("you win")
